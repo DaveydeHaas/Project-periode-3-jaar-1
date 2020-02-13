fe8e7c873ff035dcff8eb440e87068be61f9ec17
Kenny Nguyen <kenny.mboutrecht@gmail.com>
2020-02-13 13:53:43 +0100

    Testcommit

--- Layout-Basic/footer.php
+++ Layout-Basic/footer.php
@@ -1,107 +1,106 @@
-<!-- Footer -->
-<footer class="footer-background-color page-footer font-small stylish-color-dark pt-4">
+<footer class="footer-background-color footer-fontsize page-footer font-small pt-4">
 
-  <!-- Footer Links -->
-  <div class="container text-center text-md-left">
 
-    <!-- Grid row -->
-    <div class="row">
+    <div class="container-fluid text-center text-md-left">
 
-      <!-- Grid column -->
-      <div class="col-md-4 mx-auto">
 
-        <!-- Content -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Schizopedia</h5>
-        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-          consectetur
-          adipisicing elit.</p>
+        <div class="row">
 
-      </div>
-      <!-- Grid column -->
 
-      <hr class="clearfix w-100 d-md-none">
+            <div class="col-md-4 mx-auto">
 
-      <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Schizopedia</h5>
+                <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipisicing elit.</p>
 
-        <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+            </div>
 
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
 
-      </div>
-      <!-- Grid column -->
+            <hr class="clearfix w-100 d-md-none">
 
-      <hr class="clearfix w-100 d-md-none">
+            <div class="col-md-2 mx-auto">
 
-      <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
 
-        <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 4</a>
+                    </li>
+                </ul>
 
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
+            </div>
 
-      </div>
-      <!-- Grid column -->
 
-      <hr class="clearfix w-100 d-md-none">
+            <hr class="clearfix w-100 d-md-none">
 
-      <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
 
-        <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+            <div class="col-md-2 mx-auto">
 
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
 
-      </div>
-      <!-- Grid column -->
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 4</a>
+                    </li>
+                </ul>
+
+            </div>
+
+
+            <hr class="clearfix w-100 d-md-none">
+
+
+            <div class="col-md-2 mx-auto">
+
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!">Link 4</a>
+                    </li>
+                </ul>
+
+            </div>
+
+
+        </div>
+
 
     </div>
-    <!-- Grid row -->
 
-  </div>
+
+    <div class="footer-copyright footer-background-color2 text-center py-3">Project 3 |
+        <a href="">Schizopedia.com</a>
+    </div>
 
 
 </footer>
-<!-- Footer -->
+</div>
