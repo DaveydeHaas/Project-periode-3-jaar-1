a3a386f11c75dea42e85bf1c82dd9e5e212bb785
Davey <326849@student.mboutrecht.nl>
2020-02-13 12:24:22 +0100

    first setup

---
+++ Layout-Basic/navbar.php
@@ -0,0 +1,25 @@
+<?php $content = (isset($_GET['content'])?$_GET['content']:false); ?>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <div class="row">
+      <a class="navbar-brand <?php if ($content == 'home') echo 'active'?>" href="index.php?content=home"><img
+          src="./img/logo.png" alt="Homepage Logo"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="<?php if ($content == 'home') echo 'active'?>">
+            <a href="./index.php?content=home">Home</a></li>
+          <li class="<?php if ($content == 'handleiding') echo 'active' ?>">
+            <a href="index.php?content=handleiding">Handleiding</a></li>
+          <li class="<?php if ($content == 'spel') echo 'active' ?>">
+            <a href="index.php?content=spel">Spel</a></li>
+          <li class="<?php if ($content == 'highscores') echo 'active' ?>">
+            <a href="index.php?content=highscores">Highscores</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,2 +1,48 @@
-Test
-Test2
+<?php ob_start(); session_start(); ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Lettertype -->
+  <link href="https://fonts.googleapis.com/css?family=Lateef&display=swap" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- Eigen stylesheet -->
+  <link rel="stylesheet" href="./css/style.css">
+  <!-- Tabblad Naam -->
+  <title>Schizopedia.org</title>
+</head>
+
+<body>
+  <!-- include navbar -->
+  <?php include("./layout-basic/navbar.php"); ?>
+
+  <!-- Content wisselen door middel van navbar -->
+  <?php
+  if (isset($_GET["content"])) {
+    include("./layout-content/" . $_GET["content"] . ".php");
+  } 
+   else {
+    include("./layout-content/home.php");
+  }
+  ?>
+
+  <!-- include footer -->
+  <?php include("./layout-basic/footer.php"); ?>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
