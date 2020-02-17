91f9dec1687eb143604f4d79bc4b8500045b4ab3
Kenny Nguyen <kenny.mboutrecht@gmail.com>
2020-02-17 14:10:28 +0100

    homepage carousel

--- Layout-Content/home.php
+++ Layout-Content/home.php
@@ -1,6 +1,42 @@
 <div class="container-fluid">
   <div class="row">
 
+
+    <div class="col-2">
+    </div>
+    <div class="col-8 home-margin-top">
+
+      <div id="carouselExampleIndicators" class="carousel slide home-carousel-border" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./img/banner1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/banner2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/banner3.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <div class="col-2">
+    </div>
+
+
     <div class="col-2">
     </div>
     <div class="col-8">
@@ -11,42 +47,41 @@
     <div class="col-2">
     </div>
     <div class="col-12 text-center">
-        <a href="index.php?content=info" class="btn btn-outline-dark home-margin-bottom">Meer informatie</a>
+      <a href="index.php?content=info" class="btn btn-outline-dark home-margin-bottom">Meer informatie</a>
     </div>
 
     <div class="col-2">
     </div>
     <div class="col-8">
-    <div class="card-deck home-margin-bottom">
-  <div class="card text-center">
-    <div class="card-body">
-      <h5 class="card-title">Handleiding</h5>
-      <p class="card-text">test</p>
-    </div>
-    <div class="card-footer">
-      <a href="index.php?content=info" class="btn btn-outline-dark">Game Info</a>
-    </div>
-  </div>
-  <div class="card text-center">
-    <div class="card-body">
-      <h5 class="card-title">Game</h5>
-      <p class="card-text">test</p>
-    </div>
-    <div class="card-footer">
-    <a href="index.php?content=info" class="btn btn-outline-dark">Speel Game</a>
-    </div>
-  </div>
-  <div class="card text-center">
-    <div class="card-body">
-      <h5 class="card-title">Highscores</h5>
-      <p class="card-text">test</p>
-    </div>
-    <div class="card-footer">
-    <a href="index.php?content=info" class="btn btn-outline-dark">Game Scores</a>
-    </div>
-  </div>
-</div>
+      <div class="card-deck home-margin-bottom">
+        <div class="card text-center">
+          <div class="card-body">
+            <h5 class="card-title">Handleiding</h5>
+            <p class="card-text">test</p>
+          </div>
+          <div class="card-footer">
+            <a href="index.php?content=info" class="btn btn-outline-dark">Game Info</a>
+          </div>
+        </div>
+        <div class="card text-center">
+          <div class="card-body">
+            <h5 class="card-title">Game</h5>
+            <p class="card-text">test</p>
+          </div>
+          <div class="card-footer">
+            <a href="index.php?content=info" class="btn btn-outline-dark">Speel Game</a>
+          </div>
+        </div>
+        <div class="card text-center">
+          <div class="card-body">
+            <h5 class="card-title">Highscores</h5>
+            <p class="card-text">test</p>
+          </div>
+          <div class="card-footer">
+            <a href="index.php?content=info" class="btn btn-outline-dark">Game Scores</a>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="col-2">
     </div>
-  
