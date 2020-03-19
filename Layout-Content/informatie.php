7615667b5c6914276bb9f80ef3d79decdf0908d1
Phew <phew2002@gmail.com>
2020-03-19 13:26:47 +0100

    Info 2.0

--- Layout-Content/informatie.php
+++ Layout-Content/informatie.php
@@ -1,29 +1,53 @@
 <div class="container-fluid">
   <div class="row">
 
-<div class="col-1"></div>
 
-<div class="list-group col-2" id="myList" role="tablist">
-  <a class="list-group-item list-group-item-action active" data-toggle="list" href="#home" role="tab">Home</a>
-  <a class="list-group-item list-group-item-action" data-toggle="list" href="#profile" role="tab">Profile</a>
-  <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages" role="tab">Messages</a>
-  <a class="list-group-item list-group-item-action" data-toggle="list" href="#settings" role="tab">Settings</a>
+    <div class="col-2"></div>
+
+    <div class="col-8">
+    <button type="button" class="btn btn-outline-danger href" href="#behandeling">Danger</button>
+    </div>
+
+
+
+    <div class="col-2"></div>
+    <div class="col-2"></div>
+
+    <div class="col-8 info-margin-bot">
+      <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+        <br>
+        <br>
+        <li class="list-group-item" id="oorzaak">
+          <h4>Oorzaak</h4>
+          Bij de oorzaak van schizofrenie speelt erfelijkheid een rol, maar genetische aanleg alleen is niet de enige factor. Het gaat ook om belastende omstandigheden (stressvolle situaties) waarin iemand dusdanig kwetsbaar is dat een psychose de kans krijgt. Schizofrenie openbaart zich vaak rond de puberteit, een tijd van grote persoonlijke veranderingen.
+        </li>
+        <li class="list-group-item" id="symtomen">
+          <h4>Symptomen</h4>
+          De meest kenmerkende verschijnselen zijn wanen, hallucinaties, het wegvallen van de samenhang in spreken en denken, onlogische gedragingen en een gebrek aan emoties en motivatie. Minstens twee van deze symptomen moeten gedurende minimaal een maand optreden.
+          <br>
+          <br>
+          De verschijningsvormen kunnen zeer verschillend zijn. Wanneer die verschijnselen in een periode samenkomen, spreken we van een psychose. Steeds terugkerende psychoses verdelen het leven van schizofreniepatiënten vaak in ‘goede en slechte periodes’.
+          <br>
+          <br>
+          Omdat de symptomen en verschijnselen zo divers zijn, komt er steeds meer discussie over de term schizofrenie. Tegenstanders hebben het liever over psychosegevoeligheid omdat schizofrenie een stempel is waar mensen moeilijk vanaf komen. Psychosegevoeligheid is dan een meer hoopgevende term. Op dit moment wordt de diagnose schizofrenie echter nog veel gebruikt en veel mensen hebben in het verleden deze diagnose gekregen.
+          <br>
+          <br>
+          Overigens wordt bij schizofrenie vaak ten onrechte gesproken van een ‘gespleten persoonlijkheid’; dit is echter toe te schrijven aan een andere stoornis (Meervoudige Persoonlijkheid Stoornis, MPS).
+        </li>
+        <li class="list-group-item" id="behandeling">
+          <h4>Behandeling</h4>
+          De behandeling vindt plaats in de geestelijke gezondheidszorg (GGZ). Medicijnen, de zogenaamde antipsychotica, bestrijden effectief de symptomen maar bieden geen genezing. Helaas is het blijven slikken van medicijnen niet voor iedere schizofreniepatiënt goed vol te houden, onder andere vanwege de bijwerkingen. Wanneer patiënten de voortekenen van een psychose bij zichzelf leren herkennen, kunnen ze tijdig hulp zoeken. Daarom hoort goede voorlichting bij de behandeling.
+        </li>
+        <li class="list-group-item" id="test4">
+          <h4>Test</h4>
+          Test
+        </li>
+
+      </div>
+    </div>
+
+    <div class="col-2"></div>
+
+
+  </div>
 </div>
-
-<div class="tab-content col-8">
-  <div class="tab-pane active" id="home" role="tabpanel">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur quidem quaerat ipsa totam, odit quod adipisci tempore nemo, sequi ducimus placeat repudiandae dolorum itaque incidunt.</div>
-  <div class="tab-pane" id="profile" role="tabpanel">..2.</div>
-  <div class="tab-pane" id="messages" role="tabpanel">3...</div>
-  <div class="tab-pane" id="settings" role="tabpanel">.4..</div>
-</div>
-
-<div class="col-1"></div>
-
-</div>
-</div>
-
-<script>
-  $(function () {
-    $('#myList a:last-child').tab('show')
-  })
-</script>
